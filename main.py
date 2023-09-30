f3e3a1781b2534d666e6d088c32520e7772af29e
0xandydev <andy@0xandy.dev>
2023-10-01 11:58:33 +1300

    major changes

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-import numpy
-print ("Hello Worlds, and world")
+#!/usr/bin/env python
+import subprocess
+
+interface = "eth0"
+new_mac = "08:00:27:b6:d2:aa"
+
+print ("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
+
